8529b6b408ac66ca74c339fd4fb34727db547206
Ana <36892125+nanuk22@users.noreply.github.com>
2022-05-09 15:33:05 +0000

    add methods

--- myApp.js
+++ myApp.js
@@ -1,5 +1,24 @@
 let express = require('express');
+console.log("Hello World");
+
 let app = express();
+// app.get("/", function(req, res) {
+//         res.send('Hello Express');
+// });
+
+const absolutePathToHtml = __dirname + "/views/index.html";
+const absolutePathToAssets = __dirname + "/public";
+
+app.get("/", function(req, res) {
+  res.sendFile(absolutePathToHtml);
+});
+
+app.use("/public", express.static(absolutePathToAssets));
+
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"});
+});
+
 
 
 
